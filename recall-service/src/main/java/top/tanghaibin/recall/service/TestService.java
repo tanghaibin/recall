1b6abe443526ff3fcdb7a0781258621a37370baf
tanghaibin <tanghaibinhaohan@163.com>
2016-04-19 16:00:35 +0800

    cmmmit

---
+++ recall-service/src/main/java/top/tanghaibin/recall/service/TestService.java
@@ -0,0 +1,7 @@
+package top.tanghaibin.recall.service;
+
+/**
+ * Created by Administrator on 2016/4/19.
+ */
+public interface TestService {
+}
---
+++ recall-war/src/main/java/top/tanghaibin/recall/controller/IndexController.java
@@ -0,0 +1,17 @@
+package top.tanghaibin.recall.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2016/4/19.
+ */
+@Controller
+@RequestMapping("index")
+public class IndexController {
+
+    private Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+}
