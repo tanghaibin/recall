ae573639164a9f48b9463616beb0664b12fdc1c0
tanghaibin <tanghaibinhaohan@163.com>
2016-04-19 15:45:58 +0800

    commit

---
+++ recall-war/src/main/java/top/tanghaibin/recall/controller/IndexController.java
@@ -0,0 +1,14 @@
+package top.tanghaibin.recall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2016/4/19.
+ */
+@Controller
+@RequestMapping("index")
+public class IndexController {
+
+
+}
